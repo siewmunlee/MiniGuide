b667652c4d637dca5d9f1ef2b9f39eef32e0edde
siewmuncra <siewmun.cra@hotmail.com>
2019-03-03 23:19:23 +0800

    init commit

---
+++ MiniGuide/app/src/main/java/com/miniguide/user/miniguide/MapsActivity.java
@@ -0,0 +1,56 @@
+package com.miniguide.user.miniguide;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    // User location
+    final LatLng userLoc = new LatLng(1.443828, 103.786199);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.setMinZoomPreference(16.25f);
+        mMap.setMaxZoomPreference(17.00f);
+
+        mMap.addMarker(new MarkerOptions().position(userLoc).title("You are here")).setTag(1);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 16.5f));
+    }
+
+//    @Override
+//    public boolean onMarkerClick(Marker marker) {
+//        return false;
+//    }
+}
