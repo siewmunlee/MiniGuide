fd7cdf91a614f62bb6b08d8e631d5a91fb9013b0
team11.rafi <rafiyuki786@gmail.com>
2019-03-05 14:42:54 +0800

    sorry sm

---
+++ MiniGuide/app/src/main/java/com/miniguide/user/miniguide/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.miniguide.user.miniguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
+
+
